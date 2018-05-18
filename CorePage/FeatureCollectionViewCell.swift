cba20a4c35b49de93c600493d8c161e6654bc049
Tischuk, Christopher <ctischuk@edmunds.com>
2018-05-18 09:55:46 -0700

    Initial commit

---
+++ CorePage/FeatureCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  FeatureCollectionViewCell.swift
+//  CorePage
+//
+//  Created by Tischuk, Christopher on 5/2/18.
+//  Copyright © 2018 Tischuk, Christopher. All rights reserved.
+//
+
+import UIKit
+
+class FeatureCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var featureImageView: UIImageView!
+    @IBOutlet weak var featureTitleLabel: UILabel!
+    
+}
--- CorePage/ViewController.swift
+++ CorePage/ViewController.swift
@@ -8,18 +8,52 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource {
 
+    @IBOutlet var buttons: [UIButton]!
+    @IBOutlet weak var featuresAndSpecsView: UIView!
+    @IBOutlet weak var featuresCollectionView: UICollectionView!
+    @IBOutlet weak var buttonVisualEffectView: UIVisualEffectView!
+    
+    private let features = [("All Wheel Drive", #imageLiteral(resourceName: "AWD")), ("Cooled Seats", #imageLiteral(resourceName: "Cooled Seats")), ("Blind Spot Monitoring", #imageLiteral(resourceName: "Blind Spot Monitoring")), ("Full LED Headlights", #imageLiteral(resourceName: "LED Headlights"))]
+    private let featureCellIdentifier = "featureCell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        buttonVisualEffectView.layer.cornerRadius = 4
+        buttons.forEach { $0.layer.cornerRadius = 4 }
+        featuresAndSpecsView.layer.cornerRadius = 4
+        featuresAndSpecsView.layer.borderWidth = 1
+        featuresAndSpecsView.layer.borderColor = UIColor(red: 230/255.0, green: 230/255.0, blue: 230/255.0, alpha: 1).cgColor
+    }
+    
+    // MARK: - UICollectionViewDataSource
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return features.count
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: featureCellIdentifier, for: indexPath) as! FeatureCollectionViewCell
+        let feature = features[indexPath.item]
+        cell.featureTitleLabel.text = feature.0
+        cell.featureImageView.image = feature.1
+        return cell
     }
-
-
+    
+    // MARK: - Orientation Methods
+    
+    override var shouldAutorotate: Bool {
+        return false
+    }
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return .portrait
+    }
 }
 
